df5226334061456ee70ac7529bbde56655d8f782
sabrinagui <134548342+sabrinagui@users.noreply.github.com>
2024-01-11 21:25:44 -0300

    first

---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './maincontent.css';
+
+const MainContent = () => {
+  return (
+    <div className="main-content">
+    <img src="caminho/da/sua/foto.jpg" alt="Descrição da foto" className="centered-image" />
+  </div>
+);
+};
+
+
+export default MainContent
